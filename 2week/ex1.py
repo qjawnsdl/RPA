ec2e6ce57ff9907e9823ad78d360ad8e03037945
[qjawnsdl] <[kjsro03@naver.com]>
2024-09-12 16:29:46 +0900

    2주차 실습파일

--- 2week/ex1.py
+++ 2week/ex1.py
@@ -10,4 +10,39 @@ list_1 = list([['C1', '인공지능개론', 'R1', '3'],
             ]) 
 
 df = pd.DataFrame(list_1, columns=col_names) 
-print(df)
+print(df) 
+
+data = { 
+        '과목번호' : ['C1', 'C2', 'C3', 'C4', 'C5', 'C6'], 
+        '과목명' : ['인공지능개론', '웃음치료', '경영학', '3D 디자인', '스포츠경영', '예술의 세계'], 
+        '강의실' : ['R1', 'R2', 'R3', 'R4', 'R2', 'R3'], 
+        '시간수' : [3,2,3,4,2,1] 
+        } 
+df = pd.DataFrame(data) 
+print(df, end='\n\n') 
+
+sr_name = df['과목명'] 
+print(sr_name, end='\n\n') 
+
+sr_no = df.loc[2] 
+print(sr_no, end='\n\n') 
+
+cell_name = df.loc[2]['과목명'] 
+print(cell_name) 
+
+df.to_csv('file.csv', index=False) 
+
+a= '################' 
+print(a) 
+
+df['담당교수'] = ['홍길동', '김철수', '이영희', '박영수', '최영희', '김영수'] 
+print(df, end='\n\n') 
+
+df.loc[6] = ['C7', '통계학', 'R7', 3, '이철수'] 
+print(df, end='\n\n') 
+
+df1 = df.drop(['강의실'], axis=1) 
+print(df1, end='\n\n') 
+
+df2 = df.drop([5], axis=0) 
+print(df2, end='\n\n') 
---
+++ 2week/ex4.py
@@ -0,0 +1,13 @@
+a = 5 
+
+if a ==5: 
+    print('Right!') 
+    print('a is 5') 
+    
+elif a ==3: 
+    print('Right!') 
+    print('a is 3') 
+
+elif a!=3: 
+    print('Right!') 
+    print('a is not 3')
